91d373855649328fa2d7b1f046dabe5ff7b565a3
iLja <evilva00@gmail.com>
2024-04-05 22:32:35 +0300

    конфиг инициализирован

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,8 +1,18 @@
 package main
 
+import "app/internal/config"
+
 func main() {
-	//todo: инициализировать конфиг
+	//инициализирован конфиг
+	cfg := config.MustLoad()
+	_ = cfg
 	//todo: инициализировать логгер
 	//todo: инициализировать логику
 	//todo: запустить сервер
 }
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
